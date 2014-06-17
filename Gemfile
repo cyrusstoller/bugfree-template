24ca0063f90d0d4e0af1511c966dd9b51fa931be
Cyrus Stoller <cyrus.stoller@gmail.com>
2014-06-17 16:05:59 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+group :development do
+  gem 'rspec', '~> 3.0.0'
+  gem 'guard-rspec', '~> 4.2.10'
+
+  # # Test gems on Macintosh OS X
+  # gem 'rb-fsevent', '~> 0.9.1', :require => false
+  # gem "growl", "~> 1.0.3"	
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
+
+RSpec.configure do |config|
+  config.order = 'random'
+end
+
+# Dir[File.join(File.dirname(__FILE__), "lib" , "**.rb")].each do |f| 
+#   require f
+# end
